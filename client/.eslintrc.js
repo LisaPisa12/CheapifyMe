700d16fcbe8d67a2bbbaa5fea4c7736c79fdcf2d
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-10-10 11:01:36 +0200

    style(enforce styling): add eslint config
    Add eslint config enforcing code styling and formatting

---
+++ client/.eslintrc.js
@@ -0,0 +1,33 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error'
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': RULES.OFF
+  }
+}
