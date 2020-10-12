46d13b8abd00f37d45eb85dbab9162fa9e61e9bd
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-10-12 16:29:24 +0200

    feat(home screen): implement home screen
    Implement home screen

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,34 @@
-import Navbar from '../layout/Navbar';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <>
-      <Navbar></Navbar>
+      <section className={styles.section} data-testid="section">
+        <div className={styles.childs} data-testid="child">
+          <img
+            className={styles.image}
+            src="cheapifyme.gif"
+            data-testid="img"
+          />
+        </div>
+        <div className={styles.childs} data-testid="child">
+          <input
+            type="textbox"
+            placeholder="location"
+            className={styles.input}
+          />
+          <button className={styles.button}>
+            <img src="gps.png" className={styles.gps_icon} />
+          </button>
+        </div>
+        <div className={styles.childs} data-testid="child">
+          <img
+            className={styles.map_placeholder}
+            src="map-placeholder.jpg"
+            data-testid="map-img"
+          />
+        </div>
+      </section>
     </>
   );
 }
