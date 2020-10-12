d64c7339385aa0e088249f3e9ad75fd8caa11eaf
Erik Pastor Rios <erikpastorrios1994@gmail.com>
2020-10-12 17:11:26 +0200

    perf(app): disallow zoom
    Disallow zoom

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css';
 
 import { AppProps } from 'next/app';
+import Head from 'next/head';
+
 import AppLayout from '../layout/Container';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AppLayout>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, 
+     user-scalable=0"
+        />
+      </Head>
       <Component {...pageProps} />
     </AppLayout>
   );
